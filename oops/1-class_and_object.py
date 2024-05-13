30a908149c54d4d28fe617a928284a1c08a15c61
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-05-13 09:37:11 +0530

    class and object - 1

---
+++ oops/1-class_and_object.py
@@ -0,0 +1,40 @@
+# class person: # class
+#     # data member
+#     name = "brijesh"
+#     age = 28
+# 
+#     # member function
+#     def display(yoyo):
+#         print("Name: ", yoyo.name)
+#         print("Age: ", yoyo.age)
+# 
+# 
+# obj = person() # object
+# 
+# print(obj.name)
+# print(obj.age)
+# obj.display()
+
+# class person:
+#     # constructor
+#     def __init__(self, name, age):
+#         self.n = name
+#         self.a = age
+# 
+#     # noremal method
+#     def display(self):
+#         print("Name: ", self.n)
+#         print("Age: ", self.a)
+# 
+# b = person("brijesh", 28)
+# b.display()
+# 
+# n = person("namrata", 25)
+# n.display()
+
+
+
+# l = list()
+# l.append([1,2,3])
+# l.extend([1,2,3])
+# print(l)
